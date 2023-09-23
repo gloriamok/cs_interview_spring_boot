62b4cf726dddc22d76feae54163bfe3267cbd122
gloriamok <gloriamok15@gmail.com>
2023-09-23 15:19:42 +0900

    add: add handleUsernameNotFoundException

--- src/main/java/com/cs_interview_spring_boot/cs_interview_spring_boot/exception/GlobalExceptionHandler.java
+++ src/main/java/com/cs_interview_spring_boot/cs_interview_spring_boot/exception/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.cs_interview_spring_boot.cs_interview_spring_boot.exception;
 import com.cs_interview_spring_boot.cs_interview_spring_boot.payload.ErrorDetails;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -13,6 +14,12 @@ import java.util.Date;
 public class GlobalExceptionHandler {
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<ErrorDetails> handleResourceNotFoundException(ResourceNotFoundException exception, WebRequest webRequest) {
+        ErrorDetails errorDetails = new ErrorDetails(new Date(), exception.getMessage(), webRequest.getDescription(false));
+        return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(UsernameNotFoundException.class)
+    public ResponseEntity<ErrorDetails> handleUsernameNotFoundException(UsernameNotFoundException exception, WebRequest webRequest) {
         ErrorDetails errorDetails = new ErrorDetails(new Date(), exception.getMessage(), webRequest.getDescription(false));
         return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
     }
