e5edc70eb466cffaad87915457b0306d6dca041a
gloriamok <gloriamok15@gmail.com>
2023-09-26 18:05:54 +0900

    fix: Remove unused Classes

--- src/main/java/com/cs_interview_spring_boot/cs_interview_spring_boot/user/UserRepository.java
+++ src/main/java/com/cs_interview_spring_boot/cs_interview_spring_boot/user/UserRepository.java
@@ -1,6 +1,5 @@
 package com.cs_interview_spring_boot.cs_interview_spring_boot.user;
 
-import com.cs_interview_spring_boot.cs_interview_spring_boot.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
