b7c3fc8bfaaa535b55800e01091120452e988f9d
gloriamok <gloriamok15@gmail.com>
2023-09-26 18:05:12 +0900

    fix: Assign emptySet instead of null

--- src/main/java/com/cs_interview_spring_boot/cs_interview_spring_boot/user/CustomUserDetailsService.java
+++ src/main/java/com/cs_interview_spring_boot/cs_interview_spring_boot/user/CustomUserDetailsService.java
@@ -1,11 +1,11 @@
 package com.cs_interview_spring_boot.cs_interview_spring_boot.user;
 
-import com.cs_interview_spring_boot.cs_interview_spring_boot.user.User;
-import com.cs_interview_spring_boot.cs_interview_spring_boot.user.UserRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.Collections;
 
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
@@ -20,6 +20,6 @@ public class CustomUserDetailsService implements UserDetailsService {
     public UserDetails loadUserByUsername(String usernameOrEmail) throws UsernameNotFoundException {
         User user = userRepository.findByUsernameOrEmail(usernameOrEmail, usernameOrEmail)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found with username or email: " + usernameOrEmail));
-        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), null);
+        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), Collections.emptySet());
     }
 }
