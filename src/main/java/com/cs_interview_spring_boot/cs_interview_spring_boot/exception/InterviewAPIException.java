7cf6aa6a422c4f17cb9e7fe7d54f101b77c2d0a8
gloriamok <gloriamok15@gmail.com>
2023-09-29 22:30:39 +0900

    fix: Customize Response Status to BAD_REQUEST

--- src/main/java/com/cs_interview_spring_boot/cs_interview_spring_boot/exception/InterviewAPIException.java
+++ src/main/java/com/cs_interview_spring_boot/cs_interview_spring_boot/exception/InterviewAPIException.java
@@ -1,7 +1,9 @@
 package com.cs_interview_spring_boot.cs_interview_spring_boot.exception;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
+@ResponseStatus(value = HttpStatus.BAD_REQUEST)
 public class InterviewAPIException extends RuntimeException {
     private HttpStatus status;
     private String message;
